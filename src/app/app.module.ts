c2f68c3a2d1487eb9a678011ee6c306544de23f4
Scott LeBaron <lebaron@amazon.com>
2018-01-03 00:52:36 -0800

    add first components and router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { RoleComponent } from './role/role.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
+const appRoutes: Routes = [
+  { path: 'role', component: RoleComponent },
+  { path: '', redirectTo: '/role', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoleComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
